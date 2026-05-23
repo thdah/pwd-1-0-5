b4d1efe9eae85d88f257b350cad5170c5f7eed3f
thdah <tda4723@gmail.com>
2026-05-23 12:34:29 +0630

    composer autoload

--- hello/index.php
+++ hello/index.php
@@ -1,12 +1,18 @@
 <?php
 
-include("autoload.php");
+include("vendor/autoload.php");
 
+use Carbon\Carbon;
 use Libs\Math\Circle;
 use Support\Square;
+
+echo Carbon::now()->addDay(5);
+
+echo "<br>";
 
 $circle = new Circle;
 $circle->area(80);
 
 $square = new Square;
 $square->area(80);
+
