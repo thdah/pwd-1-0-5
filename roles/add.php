85d756d66749b04833240edc7df1cddc87dee935
thdah <tda4723@gmail.com>
2026-05-24 15:26:46 +0630

    parameter in sql

--- roles/add.php
+++ roles/add.php
@@ -3,9 +3,10 @@
 $name = $_POST['name'];
 $value = $_POST['value'];
 
-$sql = "INSERT INTO roles (name, value) VALUES ('$name', $value);";
-
 $db = new PDO('mysql:host=localhost;dbname=project', 'root', 'root');
-$db->query($sql);
+
+$sql = "INSERT INTO roles (name, value) VALUES (:name, :value)";
+$statement = $db->prepare($sql);
+$statement->execute(["name" => $name, "value" => $value]);
 
 header('location: index.php');
